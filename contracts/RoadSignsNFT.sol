18fa10b7aa8a50f264e5aa795823f6f2fb2ad935
Maksym Bielushkin <maxb@getblend.com>
2022-08-28 12:17:44 +0200

    Initial commit

---
+++ contracts/RoadSignsNFT.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.1;
+
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+
+// We need to import the helper functions from the contract that we copy/pasted.
+import { Base64 } from "./libraries/Base64.sol";
+
+contract RoadSignsNFT is ERC721URIStorage {
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  string baseSvg = "<svg width='800' height='800' fill='none' xmlns='http://www.w3.org/2000/svg'><style>.base { font-family: Lato,sans-serif; margin:100px;}</style><g><rect width='100%' height='100%' fill='none'/> <rect x='";
+
+  constructor() ERC721 ("SquareNFT", "SQUARE") {
+    console.log("Mint NFT contract");
+  }
+
+  function mintNFT(string memory city, uint x, uint width) public {
+    uint256 newItemId = _tokenIds.current();
+
+    string memory finalSvg = string(abi.encodePacked(
+      baseSvg, 
+      Strings.toString(x), 
+      "' y='350' width='",
+      Strings.toString(width), 
+      "' height='102' rx='19' fill='white'/><rect x='",
+      Strings.toString(x), 
+      "' y='350' width='",
+      Strings.toString(width),
+      "' height='102' rx='19' stroke='black' stroke-width='6'/><text x='400' y='420' fill='black' font-size='60' font-weight='bold' class='base' text-anchor='middle'>",
+      city, 
+      "</text></g></svg>"
+    ));
+
+    // Get all the JSON metadata in place and base64 encode it.
+    string memory json = Base64.encode(bytes(string(abi.encodePacked(
+      '{"name": "',
+        city,
+        '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',
+        Base64.encode(bytes(finalSvg)),
+      '"}'
+    ))));
+
+    string memory finalTokenUri = string(abi.encodePacked("data:application/json;base64,", json));
+
+    console.log("\n--------------------");
+    console.log(finalTokenUri);
+    console.log("--------------------\n");
+
+    _safeMint(msg.sender, newItemId);
+    _setTokenURI(newItemId, finalTokenUri);
+
+    _tokenIds.increment();
+    console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+  }
+}
